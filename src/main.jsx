b9093ce9c745db4f918919393ab68f10c56956f8
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-06-18 05:09:50 +0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import "./index.css";
+
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router";
+import App from "./App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
