df00600756a80f315cd7156c93ea70173d991666
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-06-18 07:33:03 +0700

    ref : landingpage

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,14 +1,22 @@
 import HeroSection from "../components/home/HeroSection";
 import FeaturesSection from "../components/home/FeaturesSection";
 import AnalyticsSection from "../components/home/AnalyticsSection";
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 function LandingPage() {
   return (
-    <>
-      <HeroSection />
-      <FeaturesSection />
-      <AnalyticsSection />
-    </>
+    <main className="flex min-h-screen flex-col bg-base-200 text-base-content">
+      <Header />
+
+      <div className="flex-1">
+        <HeroSection />
+        <FeaturesSection />
+        <AnalyticsSection />
+      </div>
+
+      <Footer />
+    </main>
   );
 }
 
